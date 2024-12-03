43c486b9f9a8114fa53b18c50f4bd02450c90e4b
cptkorea <cptkorea7@gmail.com>
2024-12-03 21:27:25 +0000

    y2024 day 1 implementation

---
+++ y2024/src/main.rs
@@ -0,0 +1,25 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Args {
+    /// Advent date (1-25)
+    #[arg(short, long)]
+    date: u8,
+
+    /// Part (1 or 2)
+    #[arg(short, long)]
+    part: u8,
+}
+
+fn main() {
+    let args = Args::parse();
+    if 1 <= args.date && args.date <= 25 {
+        match y2024::run(2024, args.date, args.part) {
+            Ok(res) => println!("Day {}, Part {}: {}", args.date, args.part, res),
+            Err(err) => eprintln!("{}", err),
+        }
+    } else {
+        println!("Invalid date {}, expected number 1-25", args.date);
+        std::process::exit(1)
+    }
+}
