8035fa7521b867063bdfee1075f47bbb875e46c3
Taejin Hwang <cptkorea7@gmail.com>
2024-12-06 15:11:20 -0800

    y2024(day6): Guard Gallivant (#14)

--- y2024/src/lib.rs
+++ y2024/src/lib.rs
@@ -8,6 +8,7 @@ mod day2;
 mod day3;
 mod day4;
 mod day5;
+mod day6;
 
 pub fn factory(date: u8) -> Box<dyn AdventProblem> {
     match date {
@@ -16,6 +17,7 @@ pub fn factory(date: u8) -> Box<dyn AdventProblem> {
         3 => Box::new(day3::Day3),
         4 => Box::new(day4::Day4),
         5 => Box::new(day5::Day5),
+        6 => Box::new(day6::Day6),
         _ => unimplemented!(),
     }
 }
