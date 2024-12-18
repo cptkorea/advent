83d59f0b95df7c45859d3f0b563791fc25fa6c71
cptkorea <cptkorea7@gmail.com>
2024-12-18 02:23:30 +0000

    y2024 day15 part2 implementation

--- y2024/src/day15/grid.rs
+++ y2024/src/day15/grid.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet, VecDeque};
+use std::collections::VecDeque;
 
 pub struct Grid {
     pub spaces: Vec<Vec<Space>>,
@@ -9,7 +9,8 @@ pub struct Grid {
 pub enum Space {
     Empty,
     Robot,
-    Box,
+    BoxLeft,
+    BoxRight,
     Wall,
 }
 
@@ -17,7 +18,7 @@ impl From<char> for Space {
     fn from(value: char) -> Self {
         match value {
             '#' => Self::Wall,
-            'O' => Self::Box,
+            'O' => Self::BoxLeft,
             '@' => Self::Robot,
             '.' => Self::Empty,
             _ => unimplemented!("unknown space type"),
@@ -25,7 +26,19 @@ impl From<char> for Space {
     }
 }
 
-#[derive(Copy, Clone, Eq, PartialEq)]
+impl Space {
+    pub fn as_char(&self) -> char {
+        match self {
+            Self::Wall => '#',
+            Self::Robot => '@',
+            Self::Empty => '.',
+            Self::BoxLeft => '[',
+            Self::BoxRight => ']',
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub enum Direction {
     Up,
     Down,
@@ -46,19 +59,20 @@ impl From<char> for Direction {
 }
 
 impl Grid {
-    pub fn move_robot(&mut self, direction: Direction) {
+    pub fn move_robot_1(&mut self, direction: Direction) {
         let space = self.robot;
-        if let Some((nr, nc)) = next_space(space, self.boundary(), direction) {
+        if let Some((nr, nc)) = self.next_space(space, direction) {
             match self.spaces[nr][nc] {
                 Space::Empty => {
                     self.spaces[space.0][space.1] = Space::Empty;
+                    self.spaces[nr][nc] = Space::Robot;
                     self.robot = (nr, nc);
                 }
-                Space::Box => {
+                Space::BoxLeft => {
                     let (mut r, mut c) = (nr, nc);
-                    while let Some((ar, ac)) = next_space((r, c), self.boundary(), direction) {
+                    while let Some((ar, ac)) = self.next_space((r, c), direction) {
                         (r, c) = (ar, ac);
-                        if self.spaces[ar][ac] == Space::Box {
+                        if self.spaces[ar][ac] == Space::BoxLeft {
                             continue;
                         } else {
                             break;
@@ -68,9 +82,12 @@ impl Grid {
                     if self.spaces[r][c] == Space::Empty {
                         self.spaces[space.0][space.1] = Space::Empty;
                         self.spaces[nr][nc] = Space::Robot;
-                        self.spaces[r][c] = Space::Box;
+                        self.spaces[r][c] = Space::BoxLeft;
                         self.robot = (nr, nc);
                     }
+                }
+                Space::BoxRight => {
+                    unimplemented!("single grid, box_right not implemented!");
                 }
                 Space::Robot => {
                     panic!("2nd robot detected on board");
@@ -80,45 +97,251 @@ impl Grid {
         }
     }
 
+    pub fn move_robot_2(&mut self, direction: Direction) {
+        let space = self.robot;
+        if let Some((nr, nc)) = self.next_space(space, direction) {
+            match self.spaces[nr][nc] {
+                Space::Empty => {
+                    self.spaces[space.0][space.1] = Space::Empty;
+                    self.spaces[nr][nc] = Space::Robot;
+                    self.robot = (nr, nc);
+                }
+                Space::BoxLeft => match direction {
+                    Direction::Right => {
+                        self.move_boxes_right((nr, nc));
+                    }
+                    Direction::Up => {
+                        self.move_boxes_up((nr, nc));
+                    }
+                    Direction::Down => {
+                        self.move_boxes_down((nr, nc));
+                    }
+                    _ => panic!("inconsistent state, visiting box-left from left"),
+                },
+                Space::BoxRight => match direction {
+                    Direction::Left => {
+                        self.move_boxes_left((nr, nc));
+                    }
+                    Direction::Up => {
+                        self.move_boxes_up((nr, nc));
+                    }
+                    Direction::Down => {
+                        self.move_boxes_down((nr, nc));
+                    }
+                    _ => panic!("inconsistent state, visiting box-right from right"),
+                },
+                Space::Robot => {
+                    panic!("2nd robot detected on board");
+                }
+                Space::Wall => return,
+            };
+        }
+    }
+
+    fn move_boxes_right(&mut self, space: (usize, usize)) {
+        let (mut r, mut c) = space;
+        while let Some((ar, ac)) = self.next_space((r, c), Direction::Right) {
+            (r, c) = (ar, ac);
+            if self.spaces[ar][ac] == Space::BoxRight || self.spaces[ar][ac] == Space::BoxLeft {
+                continue;
+            } else {
+                break;
+            }
+        }
+
+        if self.spaces[r][c] == Space::Empty {
+            for (i, col) in (space.1 + 1..=c).rev().enumerate() {
+                if i % 2 == 0 {
+                    self.spaces[r][col] = Space::BoxRight;
+                } else {
+                    self.spaces[r][col] = Space::BoxLeft;
+                }
+            }
+
+            let (robot_r, robot_c) = self.robot;
+            self.spaces[robot_r][robot_c] = Space::Empty;
+            self.spaces[robot_r][robot_c + 1] = Space::Robot;
+            self.robot = (robot_r, robot_c + 1);
+        }
+    }
+
+    fn move_boxes_left(&mut self, space: (usize, usize)) {
+        let (mut r, mut c) = space;
+        while let Some((ar, ac)) = self.next_space((r, c), Direction::Left) {
+            (r, c) = (ar, ac);
+            if self.spaces[ar][ac] == Space::BoxRight || self.spaces[ar][ac] == Space::BoxLeft {
+                continue;
+            } else {
+                break;
+            }
+        }
+
+        if self.spaces[r][c] == Space::Empty {
+            for (i, col) in (c..space.1).enumerate() {
+                if i % 2 == 0 {
+                    self.spaces[r][col] = Space::BoxLeft;
+                } else {
+                    self.spaces[r][col] = Space::BoxRight;
+                }
+            }
+
+            let (robot_r, robot_c) = self.robot;
+            self.spaces[robot_r][robot_c] = Space::Empty;
+            self.spaces[robot_r][robot_c - 1] = Space::Robot;
+            self.robot = (robot_r, robot_c - 1);
+        }
+    }
+
+    fn move_boxes_up(&mut self, (r, c): (usize, usize)) {
+        let mut boxes_to_move = VecDeque::new();
+        let mut queue = VecDeque::new();
+        queue.push_back((r, c));
+        boxes_to_move.push_back((r, c));
+
+        if self.spaces[r][c] == Space::BoxLeft {
+            queue.push_back((r, c + 1));
+            boxes_to_move.push_back((r, c + 1));
+        } else if self.spaces[r][c] == Space::BoxRight {
+            queue.push_back((r, c - 1));
+            boxes_to_move.push_back((r, c - 1));
+        }
+
+        while !queue.is_empty() {
+            let n = queue.len();
+            for _ in 0..n {
+                let (r, c) = queue.pop_front().unwrap();
+                if let Some((nr, nc)) = self.next_space((r, c), Direction::Up) {
+                    if self.spaces[nr][nc] == Space::Wall {
+                        return;
+                    } else if self.spaces[nr][nc] == Space::BoxLeft {
+                        if !boxes_to_move.contains(&(nr, nc)) {
+                            queue.push_back((nr, nc));
+                            boxes_to_move.push_back((nr, nc));
+                        }
+                        if !boxes_to_move.contains(&(nr, nc + 1)) {
+                            queue.push_back((nr, nc + 1));
+                            boxes_to_move.push_back((nr, nc + 1));
+                        }
+                    } else if self.spaces[nr][nc] == Space::BoxRight {
+                        if !boxes_to_move.contains(&(nr, nc)) {
+                            queue.push_back((nr, nc));
+                            boxes_to_move.push_back((nr, nc));
+                        }
+                        if !boxes_to_move.contains(&(nr, nc - 1)) {
+                            queue.push_back((nr, nc - 1));
+                            boxes_to_move.push_back((nr, nc - 1));
+                        }
+                    }
+                }
+            }
+        }
+
+        while let Some((r, c)) = boxes_to_move.pop_back() {
+            if self.spaces[r - 1][c] != Space::Empty {
+                panic!("trying to push box into non-empty space");
+            }
+            self.spaces[r - 1][c] = self.spaces[r][c];
+            self.spaces[r][c] = Space::Empty;
+        }
+        let (robot_r, robot_c) = self.robot;
+        self.spaces[robot_r][robot_c] = Space::Empty;
+        self.spaces[robot_r - 1][robot_c] = Space::Robot;
+        self.robot = (robot_r - 1, robot_c);
+    }
+
+    fn move_boxes_down(&mut self, (r, c): (usize, usize)) {
+        let mut boxes_to_move = VecDeque::new();
+        let mut queue = VecDeque::new();
+
+        queue.push_back((r, c));
+        boxes_to_move.push_back((r, c));
+
+        if self.spaces[r][c] == Space::BoxLeft {
+            queue.push_back((r, c + 1));
+            boxes_to_move.push_back((r, c + 1));
+        } else if self.spaces[r][c] == Space::BoxRight {
+            queue.push_back((r, c - 1));
+            boxes_to_move.push_back((r, c - 1));
+        }
+
+        while !queue.is_empty() {
+            let n = queue.len();
+            for _ in 0..n {
+                let (r, c) = queue.pop_front().unwrap();
+                if let Some((nr, nc)) = self.next_space((r, c), Direction::Down) {
+                    if self.spaces[nr][nc] == Space::Wall {
+                        return;
+                    } else if self.spaces[nr][nc] == Space::BoxLeft {
+                        if !boxes_to_move.contains(&(nr, nc)) {
+                            queue.push_back((nr, nc));
+                            boxes_to_move.push_back((nr, nc));
+                        }
+                        if !boxes_to_move.contains(&(nr, nc + 1)) {
+                            queue.push_back((nr, nc + 1));
+                            boxes_to_move.push_back((nr, nc + 1));
+                        }
+                    } else if self.spaces[nr][nc] == Space::BoxRight {
+                        if !boxes_to_move.contains(&(nr, nc)) {
+                            queue.push_back((nr, nc));
+                            boxes_to_move.push_back((nr, nc));
+                        }
+                        if !boxes_to_move.contains(&(nr, nc - 1)) {
+                            queue.push_back((nr, nc - 1));
+                            boxes_to_move.push_back((nr, nc - 1));
+                        }
+                    }
+                }
+            }
+        }
+
+        while let Some((r, c)) = boxes_to_move.pop_back() {
+            if self.spaces[r + 1][c] != Space::Empty {
+                panic!("trying to push box into non-empty space");
+            }
+            self.spaces[r + 1][c] = self.spaces[r][c];
+            self.spaces[r][c] = Space::Empty;
+        }
+        let (robot_r, robot_c) = self.robot;
+        self.spaces[robot_r][robot_c] = Space::Empty;
+        self.spaces[robot_r + 1][robot_c] = Space::Robot;
+        self.robot = (robot_r + 1, robot_c);
+    }
+
     fn boundary(&self) -> (usize, usize) {
         (self.spaces.len(), self.spaces[0].len())
     }
-}
 
-pub fn next_space(
-    space: (usize, usize),
-    boundary: (usize, usize),
-    direction: Direction,
-) -> Option<(usize, usize)> {
-    let (row, col) = space;
-    let (m, n) = boundary;
-    match direction {
-        Direction::Up => {
-            if row == 0 {
-                None
-            } else {
-                Some((row - 1, col))
+    fn next_space(&self, space: (usize, usize), direction: Direction) -> Option<(usize, usize)> {
+        let (row, col) = space;
+        let (m, n) = self.boundary();
+        match direction {
+            Direction::Up => {
+                if row == 0 {
+                    None
+                } else {
+                    Some((row - 1, col))
+                }
             }
-        }
-        Direction::Down => {
-            if row >= m - 1 {
-                None
-            } else {
-                Some((row + 1, col))
+            Direction::Down => {
+                if row >= m - 1 {
+                    None
+                } else {
+                    Some((row + 1, col))
+                }
             }
-        }
-        Direction::Left => {
-            if col == 0 {
-                None
-            } else {
-                Some((row, col - 1))
+            Direction::Left => {
+                if col == 0 {
+                    None
+                } else {
+                    Some((row, col - 1))
+                }
             }
-        }
-        Direction::Right => {
-            if col >= n - 1 {
-                None
-            } else {
-                Some((row, col + 1))
+            Direction::Right => {
+                if col >= n - 1 {
+                    None
+                } else {
+                    Some((row, col + 1))
+                }
             }
         }
     }
--- y2024/src/day15/mod.rs
+++ y2024/src/day15/mod.rs
@@ -11,33 +11,55 @@ impl AdventProblem for Day15 {
         let directions = parse_directions(&lines, i + 1);
 
         for dir in directions {
-            grid.move_robot(dir);
+            grid.move_robot_1(dir);
         }
 
         let mut total = 0;
         for (i, row) in grid.spaces.iter().enumerate() {
+            let mut line = String::new();
             for (j, space) in row.iter().enumerate() {
-                if *space == Space::Box {
+                line.push(space.as_char());
+                if *space == Space::BoxLeft {
                     total += 100 * i + j;
                 }
             }
+            println!("{}", line);
         }
 
         Ok(total as u32)
     }
 
     fn run_part_2(&self, lines: Vec<String>) -> Result<u32, AdventError> {
-        Ok(0)
+        let (mut grid, i) = parse_wide_grid(&lines);
+        let directions = parse_directions(&lines, i + 1);
+
+        for dir in directions {
+            grid.move_robot_2(dir);
+        }
+
+        let mut total = 0;
+        for (i, row) in grid.spaces.iter().enumerate() {
+            let mut line = String::new();
+            for (j, space) in row.iter().enumerate() {
+                line.push(space.as_char());
+                if *space == Space::BoxLeft {
+                    total += 100 * i + j;
+                }
+            }
+            println!("{}", line);
+        }
+
+        Ok(total as u32)
     }
 }
 
-fn parse_grid(lines: &Vec<String>) -> (Grid, usize) {
+fn parse_grid<S: AsRef<str>>(lines: &Vec<S>) -> (Grid, usize) {
     let mut spaces = Vec::new();
     let (mut i, n) = (0, lines.len());
     let mut robot = (0, 0);
 
     while i < n {
-        let line = &lines[i];
+        let line = lines[i].as_ref();
         if line.is_empty() {
             break;
         }
@@ -48,6 +70,46 @@ fn parse_grid(lines: &Vec<String>) -> (Grid, usize) {
                 robot = (i, j);
             }
             row.push(space);
+        }
+        spaces.push(row);
+        i += 1;
+    }
+
+    (Grid { spaces, robot }, i)
+}
+
+fn parse_wide_grid<S: AsRef<str>>(lines: &Vec<S>) -> (Grid, usize) {
+    let mut spaces = Vec::new();
+    let (mut i, n) = (0, lines.len());
+    let mut robot = (0, 0);
+
+    while i < n {
+        let line = lines[i].as_ref();
+        if line.is_empty() {
+            break;
+        }
+        let mut row = Vec::new();
+        for (j, c) in line.chars().enumerate() {
+            match Space::from(c) {
+                Space::Empty => {
+                    row.push(Space::Empty);
+                    row.push(Space::Empty);
+                }
+                Space::BoxLeft => {
+                    row.push(Space::BoxLeft);
+                    row.push(Space::BoxRight);
+                }
+                Space::Robot => {
+                    row.push(Space::Robot);
+                    row.push(Space::Empty);
+                    robot = (i, 2 * j);
+                }
+                Space::Wall => {
+                    row.push(Space::Wall);
+                    row.push(Space::Wall);
+                }
+                _ => panic!("inconsistent state"),
+            }
         }
         spaces.push(row);
         i += 1;
@@ -69,4 +131,75 @@ fn parse_directions(lines: &Vec<String>, start: usize) -> Vec<Direction> {
     }
 
     directions
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_push_box_left() {
+        let lines = vec![
+            "#######", "#...#.#", "#.....#", "#..OO@#", "#..O..#", "#.....#", "#######",
+        ];
+
+        let (mut grid, _) = parse_wide_grid(&lines);
+        grid.move_robot_2(Direction::Left);
+
+        let expected = vec![
+            "##############",
+            "##......##..##",
+            "##..........##",
+            "##...[][]@..##",
+            "##....[]....##",
+            "##..........##",
+            "##############",
+        ];
+
+        let mut actual = Vec::new();
+        for row in grid.spaces.iter() {
+            let mut line = String::new();
+            for space in row {
+                line.push(space.as_char());
+            }
+            actual.push(line);
+        }
+
+        for (i, &expect) in expected.iter().enumerate() {
+            assert_eq!(expect, actual[i]);
+        }
+    }
+
+    #[test]
+    fn test_push_box_up() {
+        let lines = vec![
+            "#######", "#...#.#", "#.....#", "#..OO.#", "#..O..#", "#..@..#", "#######",
+        ];
+
+        let (mut grid, _) = parse_wide_grid(&lines);
+        grid.move_robot_2(Direction::Up);
+
+        let expected = vec![
+            "##############",
+            "##......##..##",
+            "##....[]....##",
+            "##....[][]..##",
+            "##....@.....##",
+            "##..........##",
+            "##############",
+        ];
+
+        let mut actual = Vec::new();
+        for row in grid.spaces.iter() {
+            let mut line = String::new();
+            for space in row {
+                line.push(space.as_char());
+            }
+            actual.push(line);
+        }
+
+        for (i, &expect) in expected.iter().enumerate() {
+            assert_eq!(expect, actual[i]);
+        }
+    }
 }
