fed6df35349a397cc95a4caa36f8ac95a9168411
Taejin Hwang <cptkorea7@gmail.com>
2024-12-18 21:18:40 -0800

    y2024(day16): Reindeer Maze (#24)

--- y2024/src/lib.rs
+++ y2024/src/lib.rs
@@ -10,6 +10,7 @@ mod day12;
 mod day13;
 mod day14;
 mod day15;
+mod day16;
 mod day2;
 mod day3;
 mod day4;
@@ -36,6 +37,7 @@ pub fn factory(date: u8) -> Box<dyn AdventProblem> {
         13 => Box::new(day13::Day13),
         14 => Box::new(day14::Day14),
         15 => Box::new(day15::Day15),
+        16 => Box::new(day16::Day16),
         _ => unimplemented!(),
     }
 }
