ebb976e4b94dea5b888038a3c26be72c24da2064
cptkorea <cptkorea7@gmail.com>
2026-05-15 17:11:22 -0700

    2025 day 2 implementation

--- solutions/src/y2025/mod.rs
+++ solutions/src/y2025/mod.rs
@@ -1,6 +1,6 @@
 use super::{AdventError, AdventProblem};
 
-advent_common::define_advent_registry!(1);
+advent_common::define_advent_registry!(1, 2);
 
 pub(crate) fn run_with_lines(lines: Vec<String>, date: u8, part: u8) -> Result<u32, AdventError> {
     factory(date).run(lines, part)
